b9dde08469614be7e60ed032ebdc87ef9c7e49cc
EngrSaeedWazir <saeedwazir313@yahoo.com>
2023-01-15 17:16:59 +0500

    ThirdCommitAddCAP+GRU+PK+EAK 1-15-23

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,31 +1,40 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL ||
+    "https://eth-goerli.g.alchemy.com/v2/7LP0D8QZiAo2JnyLF5hjMo7LNhzVAq-x"
+const PRIVATE_KEY =
+    process.env.PRIVATE_KEY ||
+    "0xa8d435457ecc795edebcc91ed28f5550c4ce4a1239c280c17149be1bfb644345"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 31337,
-      // gasPrice: 130000000000,
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            // gasPrice: 130000000000,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6,
+        },
     },
-    goerli: {
-      url: GOERLI_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 5,
-      blockConfirmations: 6,
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: COINMARKETCAP_API_KEY,
     },
-  },
-  gasReporter: {
-    enabled: true,
-    currency: "USD",
-    outputFile: "gas-report.txt",
-    noColors: true,
-    // coinmarketcap: COINMARKETCAP_API_KEY,
-  },
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
-    // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
-  },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+        // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
+    },
 
-  solidity: "0.8.17",
-};
+    solidity: "0.8.17",
+}
