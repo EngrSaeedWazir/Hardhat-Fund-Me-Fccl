6588868931e8050347b48f406700062ff81fda23
EngrSaeedWazir <saeedwazir313@yahoo.com>
2023-01-17 10:09:44 +0500

    SecondCommitAddhardhatNAccSolVer 1-16-23

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
+require("hardhat-deploy")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
@@ -31,10 +32,16 @@ module.exports = {
         noColors: true,
         // coinmarketcap: COINMARKETCAP_API_KEY,
     },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+    },
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
         // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
     },
 
-    solidity: "0.8.17",
+    solidity: { compilers: [{ version: "0.8.17" }, { version: "0.6.6" }] },
 }
