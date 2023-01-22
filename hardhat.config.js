032aa451a6ff23440d81f3427c28e5d5e3a4c83c
EngrSaeedWazir <saeedwazir313@yahoo.com>
2023-01-22 12:24:49 +0500

    EighteenCommitAddToken 1-22-13

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,33 +15,34 @@ module.exports = {
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
-            chainId: 31337,
+            chainId: 31337
             // gasPrice: 130000000000,
         },
         goerli: {
             url: GOERLI_RPC_URL,
             accounts: [PRIVATE_KEY],
             chainId: 5,
-            blockConfirmations: 6,
-        },
+            blockConfirmations: 6
+        }
     },
     gasReporter: {
         enabled: true,
         currency: "USD",
         outputFile: "gas-report.txt",
         noColors: true,
-        // coinmarketcap: COINMARKETCAP_API_KEY,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "ETH"
     },
     namedAccounts: {
         deployer: {
             default: 0, // here this will by default take the first account as deployer
-            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
-        },
+            1: 0 // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        }
     },
     etherscan: {
-        apiKey: ETHERSCAN_API_KEY,
+        apiKey: ETHERSCAN_API_KEY
         // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
     },
 
-    solidity: { compilers: [{ version: "0.8.17" }, { version: "0.6.6" }] },
+    solidity: { compilers: [{ version: "0.8.17" }, { version: "0.6.6" }] }
 }
